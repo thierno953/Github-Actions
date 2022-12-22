d96df2e8d4fc1d1a83aa3b8fbc13c49db271d212
thierno953 <thiernobarry554@gmail.com>
2022-12-22 19:49:29 +0100

    add github actions

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:erbium-alpine
+
+WORKDIR /usr/src/app
+
+COPY ./ /usr/src/app/
+
+RUN npm install 
+
+EXPOSE 3000
+
+CMD ["sh", "-c", "date; npm start "]
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const pjson = require('./package.json');
+
+app.use(morgan('dev'));
+
+app.get('/getenv/*', function (req, res) {
+    const parts = req.url.split('/');
+    var msg = "";
+
+    if (parts[2]) {
+        var variable = parts[2]
+        msg = `\nENVIRONMENT ${variable}\n` + process.env[variable];
+    } else {
+        msg = '\NnENVIRONMENT VALUES\n' + JSON.stringify(process.env, null, 4);
+    }
+
+    res.send(req.url + msg);
+    console.log(msg);
+});
+
+app.get('/*', function (req, res) {
+    res.send('Hello There Github actions' + '<hr>package json:' + pjson.version);
+});
+
+const APP_PORT = process.env.PORT || 3000;
+
+const listen = app.listen(APP_PORT, function () {
+    console.log(`Listening on port ` + listen.address().port);
+});
